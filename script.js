7ad46d9218a1ba417e990cd8065c7008dae8ebf4
Rafael <98551688+etzoider@users.noreply.github.com>
2023-03-07 18:26:28 -0300

    Finished styling and started script, created class book && class library

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+bookGrid = document.querySelector('.main');
 
 class Book {
     constructor(title, author, wasRead) {
@@ -11,8 +11,9 @@ class Book {
 }
 
 class Library {
-    constructor(bookList = []) {
+    constructor(bookList = [], bookGrid) {
         this.bookList = bookList;
+        this.bookGrid = bookGrid;
     }
 
     contains(bookTitle) {
@@ -26,8 +27,9 @@ class Library {
 
     appendBook(title, author, wasRead) {
         this.bookList.push(new Book(title, author, wasRead));
+        this.updateBookGrid(bookGrid);
     }
-
+    
     removeBook(title) {
         if (!this.contains(title)) {
             return;
@@ -36,12 +38,69 @@ class Library {
             let current = this.bookList[i];
             if (current.title == title) {
                 this.bookList.slice(i, 1);
+                updateBookGrid(bookGrid);
                 return
             }
         }
     }
 
+    createDivFromBook(book) {
+        bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        if (book.wasRead == true) {
+            bookCard.classList.add('read');
+        }
+        let title = document.createElement('div')
+        title.classList.add('title');
+        title.textContent = book.title;
+
+        let author = document.createElement('div');
+        author.classList.add('author');
+        author.textContent = book.author;
+
+        let cardButtons = document.createElement('div');
+        cardButtons.classList.add('card-buttons');
+
+        let toggleReadBtn = document.createElement('button');
+        toggleReadBtn.classList.add('toggle-read-btn');
+
+        let wasReadDisplay=  document.createElement('span');
+        wasReadDisplay.classList.add('wasRead-display')
+        wasReadDisplay.textContent = 'read';
+
+        let deleteBookBtn = document.createElement('button');
+        deleteBookBtn.classList.add('delete-book-btn');
+
+        let deleteIcon = document.createElement('span');
+        deleteIcon.classList.add('materials-symbols-outlined');
+        deleteIcon.textContent = 'delete';
+
+        toggleReadBtn.appendChild(wasReadDisplay);
+        deleteBookBtn.appendChild(deleteIcon);
+        cardButtons.appendChild(toggleReadBtn);
+        cardButtons.appendChild(deleteBookBtn);
+
+        bookCard.appendChild(title);
+        bookCard.appendBook(author);
+        bookCard.appendChild(cardButtons);
+        
+        return bookCard;
+    }
+
+    updateBookGrid(bookGrid) {
+        while (bookGrid.firstChild) {
+            bookGrid.children.slice(0,1);
+        }
+        this.bookList.forEach(book => {
+            let bookItemDiv = createDivFromBook(book);
+            bookGrid.children.push(bookItemDiv)
+        })
+    }
+
 }
+
+
+
 
 function openGithub() {
     open('https://github.com/etzoider');
@@ -67,7 +126,6 @@ function handlePlusBookClick() {
     bookGrid.appendChild(newBook);
 }
 
-bookGrid = document.querySelector('.main');
 function handleMinusBookClick() {
     alert(bookGrid.lastChild)
     bookGrid.removeChild(bookGrid.lastChild);
